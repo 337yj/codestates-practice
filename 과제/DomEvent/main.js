876f5b31e66f139e8fad60cb76c17f44c21fa760
337yj <337yje@gmail.com>
2023-02-02 19:19:42 +0900

    커링을 활용해서 onclick을 구현

--- 과제/DomEvent/main.js
+++ 과제/DomEvent/main.js
@@ -1,21 +1,32 @@
-const downButton = document.querySelector(".down-button");
-const upButton = document.querySelector(".up-button");
-const counterView = document.querySelector(".counter-view");
+const downBtn = document.querySelector(".downBtn");
+const upBtn = document.querySelector(".upBtn");
+const num = document.querySelector(".num");
 
-let counter = 0;
-const countUP = () => {
-  counter++;
-  counterView.innerHTML = counter;
-}
+let count = 0;
 
-const countDown = () => {
-  if(counter <= 0){
-    return alert("0보다 작아질 순 없어요 🐻");
-  }
-  counter--;
-  counterView.innerHTML = counter;
-}
+num.innerText = `${count}`.padStart(2,0);
 
-upButton.onclick = countUP;
-downButton.onclick = countDown;
+// 커링을 활용해서 onclick을 구현
+// type ("down", "up")
+const onClickButton = (type) => {
+  return () => {
+    if(type === "down"){
+      if(count === 0){
+        alert("0보다 작을 순 없어요 🐻");
+        return;
+      }
+      count--;
+      const targetCount = count;
+      num.innerText = `${targetCount}`.padStart(2, "0");
+    }
+    else if (type === "up") {
+      count++; // 1을 증가
+      const targetCount = count; // targetCount에 count를 할당
 
+      num.innerText = `${targetCount}`.padStart(2, "0");
+    } else alert("Down 혹은 Up을 입력해주세요.");
+  };
+};
+
+downBtn.onclick = onClickButton("down");
+upBtn.onclick = onClickButton("up");
